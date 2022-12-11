4743ae80a800c447181fe0f1205438989fa56a8c
julian10m <35936197+julian10m@users.noreply.github.com>
2022-12-11 15:55:33 +0100

    rectangleTarget: changes to reflect that x and y are now the center of the rectangle

--- ShootingGallery/rectangleTarget.lua
+++ ShootingGallery/rectangleTarget.lua
@@ -3,6 +3,7 @@ local Target = require("target")
 local RectangleTarget = Target:new()
 RectangleTarget.width = TARGET_WIDTH
 RectangleTarget.height = TARGET_HEIGHT
+RectangleTarget.angle = TARGET_ROTATION_ANGLE
 
 function RectangleTarget:new(width, height)
     local t = Target.new(self)
@@ -12,27 +13,31 @@ function RectangleTarget:new(width, height)
             t.height = height
         end
     end
+    -- t.angle = math.pi * math.random()
     return t
 end
 
 function RectangleTarget:place(x, y)
-    self.x = x or math.random(0, graphics.getWidth() - self.width)
-    self.y = y or math.random(0, graphics.getHeight() - self.height)
+    self.x = x or math.random(self.width / 2, graphics.getWidth() - self.width / 2)
+    self.y = y or math.random(self.height / 2, graphics.getHeight() - self.height / 2)
 end
 
 function RectangleTarget:draw()
     graphics.setColor(1, 0, 0)
     graphics.push()
     graphics.translate(self.x, self.y)
-    graphics.rotate(0)
-    graphics.rectangle("fill", 0, 0, self.width, self.height)
-    -- graphics.rectangle("fill", -self.width / 2, -self.height / 2, self.width, self.height) -- origin in the middle
+    graphics.rotate(self.angle)
+    -- graphics.rectangle("fill", 0, 0, self.width, self.height)
+    graphics.rectangle("fill", -self.width / 2, -self.height / 2, self.width, self.height) -- origin in the middle
     graphics.pop()
     graphics.setColor(1, 1, 1)
 end
 
 function RectangleTarget:isHit(x, y)
-    return self.x <= x and x <= self.x + self.width and self.y <= y and y <= self.y + self.height
+    return self.x - self.width / 2 <= x and
+        x <= self.x + self.width / 2 and
+        self.y - self.height / 2 <= y and
+        y <= self.y + self.height / 2
 end
 
 return RectangleTarget
