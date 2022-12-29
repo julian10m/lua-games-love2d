0a768ddab04edf602d2450918dc46fe0cb21d516
julian10m <35936197+julian10m@users.noreply.github.com>
2022-12-29 10:36:13 +0100

    target: adding direction other than 45 degrees

--- ShootingGallery/targets/target.lua
+++ ShootingGallery/targets/target.lua
@@ -4,8 +4,8 @@ function Target:new()
     local t = {}
     setmetatable(t, self)
     self.__index = self
-    t.xSpeed = 2 * (math.random() < 0.5 and -1 or 1)
-    t.ySpeed = 2 * (math.random() < 0.5 and -1 or 1)
+    t.xSpeed = math.random(10, 200) * (math.random() < 0.5 and -1 or 1)
+    t.ySpeed = math.random(10, 200) * (math.random() < 0.5 and -1 or 1)
     return t
 end
 
@@ -14,16 +14,16 @@ function Target:update(dt)
 end
 
 function Target:updateCenterPosition(dt)
-    local nextX = self.x + self.xSpeed
-    local nextY = self.y + self.ySpeed
+    local nextX = self.x + self.xSpeed * dt
+    local nextY = self.y + self.ySpeed * dt
     if nextX < 0 or nextX > graphics.getWidth() then
         self.xSpeed = -1 * self.xSpeed
     end
     if nextY < 0 or nextY >= graphics.getHeight() then
         self.ySpeed = -1 * self.ySpeed
     end
-    self.x = self.x + self.xSpeed
-    self.y = self.y + self.ySpeed
+    self.x = self.x + self.xSpeed * dt
+    self.y = self.y + self.ySpeed * dt
 end
 
 function Target:place(x, y) end
