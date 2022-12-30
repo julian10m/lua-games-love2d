13276f60873066db9c329a8973ba0f2334fbd974
julian10m <35936197+julian10m@users.noreply.github.com>
2022-12-30 12:31:45 +0100

    circle: add comments

--- ShootingGallery/targets/circleTarget.lua
+++ ShootingGallery/targets/circleTarget.lua
@@ -6,6 +6,9 @@ local CircleTarget = Target:new()
 CircleTarget.minRadius = TARGET_MIN_RADIUS_CIRC
 CircleTarget.maxRadius = TARGET_MAX_RADIUS_CIRC
 
+---Constructor returning a new circle target object.
+---@param radius number
+---@return CircleTarget
 function CircleTarget:new(radius)
     local t = Target.new(self)
     if radius then
@@ -16,17 +19,25 @@ function CircleTarget:new(radius)
     return t
 end
 
+---Sets the position of the target
+---@param x number
+---@param y number
 function CircleTarget:place(x, y)
     self.x = x or math.random(self.radius, graphics.getWidth() - self.radius)
     self.y = y or math.random(self.radius, graphics.getHeight() - self.radius)
 end
 
+---Draws the target on the game
 function CircleTarget:draw()
     graphics.setColor(1, 0, 0)
     graphics.circle("fill", self.x, self.y, self.radius)
     -- graphics.draw(sprites.circleTarget, self.x - self.radius, self.y - self.radius)
 end
 
+---Tells whether a point (x, y) touches the target
+---@param x number
+---@param y number
+---@return boolean
 function CircleTarget:isHit(x, y)
     return distanceBetween(x, y, self.x, self.y) < self.radius
 end
