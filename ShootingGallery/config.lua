b366ff36e601e109b8c3accaae4430c5b15ed3b6
julian10m <35936197+julian10m@users.noreply.github.com>
2023-01-01 23:49:21 +0100

    add docs to global funcs in config

--- ShootingGallery/config.lua
+++ ShootingGallery/config.lua
@@ -35,10 +35,20 @@ sprites = {
     -- circleTarget = graphics.newImage("sprites/target.png"),
 }
 
+---Returns the Euclidean distance between points (x1, y1) and (x2, y2)
+---@param x1 number
+---@param y1 number
+---@param x2 number
+---@param y2 number
+---@return number
 function distanceBetween(x1, y1, x2, y2)
     return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 end
 
+---Rounds a number to a given number of digits.
+---@param value number
+---@param digits number
+---@return number
 function roundValue(value, digits)
     local inverse = 1 / (10 ^ digits)
     return value - value % inverse
