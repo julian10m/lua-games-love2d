6942782741b6d0d935e3ea33c070956633c7ea07
julian10m <35936197+julian10m@users.noreply.github.com>
2023-01-07 17:33:46 +0100

    enemy: fix sprite and made global until refactoring

--- Platformer/enemy.lua
+++ Platformer/enemy.lua
@@ -1,4 +1,4 @@
-local enemies = {}
+enemies = {}
 
 function spawnEnemy(x, y)
     local enemy = world:newRectangleCollider(x, y, 70, 90, { collision_class = COLLISION_CLASSES.Danger })
@@ -12,20 +12,19 @@ function updateEnemies(dt)
     for i, e in ipairs(enemies) do
         e.animation:update(dt)
         local ex, ey = e:getPosition()
-        local colliders = world:queryRectangleArea(ex + (40 * e.direction), ey + 40, 10, 10, { COLLISION_CLASSES.Platform })
+        local colliders = world:queryRectangleArea(ex + (40 * e.direction), ey + 40, 10, 10,
+            { COLLISION_CLASSES.Platform })
         if #colliders == 0 then
-            e.direction = - e.direction
+            e.direction = -e.direction
         end
         e:setX(ex + e.speed * dt * e.direction)
-    end 
+    end
 end
 
 function drawEnemies()
     for i, e in ipairs(enemies) do
         local ex, ey = e:getPosition()
-        e.animation:draw(sprites.playerSheet, ex, ey, nil, e.direction, 1, 50, 65)
+        e.animation:draw(sprites.enemySheet, ex, ey, nil, e.direction, 1, 50, 65)
     end
 
 end
-
-return enemies
